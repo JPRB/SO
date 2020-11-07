264414003e74fd7c1a652d79626b2f4b65cc2e01
sc0p <42301678+sc0p@users.noreply.github.com>
2020-11-07 23:12:47 +0000

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-  int pontos = 0, i = 0, pontos = 0, certas = 0, bull = 0;
+  int pontos = 0, i = 0, pontos = 0, corretas = 0, bull = 0;
 
   printf("BEM VINDO AO JOGO\n");
   printf("O objetivo deste jogo é apenas para testar as funcionalidades do sistema CHAMPION\nTente acertar no número que o computador vai gerar entre 0 e 5\n BOA SORTE!");
@@ -12,11 +12,12 @@ int main()
   while(i<5)
   {
     int randomNUM = rand();
-
+    srand(time(NULL));
     printf("Adivinhe o número gerado ...\n");
     scanf("%d\n",&bull );
     if (bull == randomNUM) {
               printf("Bingo!!!\n");
+              corretas ++;
               pontos ++;
               i++;
     }
@@ -27,6 +28,5 @@ int main()
   }
   printf("Parabens... acabou com %d pontos\n",&pontos); 
 
-strand(time(NULL));
 exit(pontos)
 }
